df62e985bbe6d18e627776431ecba630979902fe
LEVI_DEVIA <leviassale@gmail.com>
2023-01-26 15:40:30 +0000

    Add fichier

---
+++ app/app.py
@@ -0,0 +1,54 @@
+# ----------------- Write your code below this line. -------------------- #
+
+# ----------------- You do NOT need to understand what the code below does. -------------------- #
+import os
+from io import BytesIO
+
+import config as config
+from flask import Flask, render_template, request
+
+from config import config
+from hotdogclassifier import HotDogClassifier
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    PORT = os.environ.get('PORT') or 8080
+    DEBUG = os.environ.get('DEBUG') != 'TRUE'
+    app.run(host='0.0.0.0', port=PORT, debug=DEBUG)
+
+model = HotDogClassifier()
+model.load_model(config["model_weight"])
+
+@app.route("/", methods=["GET"])    #première route
+def home():
+    return render_template("index.html", flag=False,
+    project_description=config["project_description"],
+    project_name=config["project_name"])
+
+@app.route("/", methods=["POST"])   #deuxième route
+def classify():
+    uploaded_file = request.files["files"]
+    data = BytesIO(uploaded_file.read())
+    if uploaded_file.filename != "":
+        img, predicted = model.predict(data)
+    else:
+        predicted, img = "", ""
+    return render_template("index.html",
+                           predicted=predicted,
+                           img=img, flag=True,
+                           project_description=config["project_description"],
+                           project_name=config["project_name"])
+
+####CONFIGURATION DE MON FICHIER
+
+config = {
+    "model_weight": "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/hotdog-not-hotdog/data"
+                    "/model_weight.pt",
+
+    "project_name": "Détecteur Hot-dog",
+
+    "project_description": "Vous êtes-vous déjà demandé si une image contenait ou non un hotdog ?"
+                           "Ne vous inquiétez pas,"
+                           "car le détecteur d'hotdog peut le faire !"
+}
